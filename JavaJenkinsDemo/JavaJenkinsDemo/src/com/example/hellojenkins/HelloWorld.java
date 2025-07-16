92eb38eadda1ab7a4d2ccfc23bd61b3886bbeedb
Ananya2005-154 <ananyarathod405@gmail.com>
2025-07-16 15:55:43 +0530

    Add files via upload

---
+++ JavaJenkinsDemo/JavaJenkinsDemo/src/com/example/hellojenkins/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.hellojenkins;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins, GitHub, and Eclipse!");
+    }
+}
