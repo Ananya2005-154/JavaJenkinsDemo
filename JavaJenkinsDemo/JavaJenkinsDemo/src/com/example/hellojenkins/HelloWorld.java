f71d9b92f3bfeaf81406df17cd22ca6416ed4465
Ananya2005-154 <ananyarathod405@gmail.com>
2025-07-16 19:39:35 +0530

    Delete JavaJenkinsDemo/JavaJenkinsDemo directory

--- JavaJenkinsDemo/JavaJenkinsDemo/src/com/example/hellojenkins/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.example.hellojenkins;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins, GitHub, and Eclipse!");
-    }
-}
